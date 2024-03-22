073e057a7bdefc53c42ff00991138ca4c5a212bf
Remnetorp <m.remnetorp@gmail.com>
2024-03-22 13:56:19 +0100

    Fixed the algorithm, it works now. Also made some descriptions both in program and in README, added some more data input for the program.

--- README.txt
+++ README.txt
@@ -1,3 +1,9 @@
 Algorithm to find a stable matching, meaning that there does not exist two studen-company-pairs (s1,c1),(s2,c2) such taht c2 is higher ranked on s1's preference list than c1 and simultaneously s1 is higher ranked on c2's preference list than s2 (thus that both s1 and c2 would prefer to leave their current matching and create new pair). Based on given data lists predefined.
 
+The input files are quite dependent on only sending in integers, and also the order of how they are sent in. The first interger on line 1 tells how many pairs there will be, so for instance 10 pairs would be 20 lines of 10 me nand 10 women. The lines after will first have one integer showing which index/number the person has and then have the number of integers of the opposite gender to show their preferences. The first occuring index/number will always be the woman and the second occurence will be the man.
+
+Run the program by either of the following lines, depending if you want extra output or not:
+java StableMarriage.java < data-input/input1.in
+java StableMarriage.java < data-input/input1.in output
+
 
--- StableMarriage.java
+++ StableMarriage.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
-
 import java.util.ArrayList;
-import java.util.Stack;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeSet;
-import java.util.Set;
+
 
 
 public class StableMarriage{
@@ -13,29 +10,30 @@ public class StableMarriage{
     private Map<Integer,ArrayList<Integer>> menPreferences;
     private Map<Integer,ArrayList<Integer>> womenPreferences;
 
-    /* 
+    /**
      * Method readFile() takes in-files in the format where first line contains a single integer N (1 <= N <= 3000), the number
      * of students and companies. The each followig line after of the in-file contains the individuals preference lists.
      * I will add some example data in-files in the repository. 
+     * 
+     * @param output Decideds if extra information should be printed out or not in the terminal.
+     * 
      */
     private void readFile(boolean output){
         Scanner scanner = new Scanner(System.in);
         nPairs = scanner.nextInt();
-        
-        scanner.nextLine();
         String input;
         menPreferences = new HashMap<>();
         womenPreferences = new HashMap<>();
 
-        while(scanner.hasNext()){
-            input = scanner.nextLine();
-            String[] indexes = input.split(" ");
-
-            int indexOfPerson = Integer.parseInt(indexes[0]);
+        for(int i=0; i < nPairs*2; i++){
+            int indexOfPerson = scanner.nextInt();
 
             ArrayList<Integer> individualList = new ArrayList<>();
-            for(int j=1; j < indexes.length; j++){
-                individualList.add(Integer.parseInt(indexes[j]));
+            for(int j=0; j < nPairs; j++){
+                int index = scanner.nextInt();
+                if(!individualList.contains(index)){
+                    individualList.add(index);
+                }
             }
             if(!womenPreferences.containsKey(indexOfPerson)){
                 womenPreferences.put(indexOfPerson,individualList);
@@ -44,35 +42,37 @@ public class StableMarriage{
             }else{
                 break;
             }
-        }
+        } 
 
-        // Print the preferences to make sure it works
+        // Print the preferences to make sure it works, call it by sending "output" in the argument when running from terminal.
         if(output){
             System.out.println("\nMen:");
             menPreferences.forEach((key, value) -> System.out.println(key + ": " + value));
             System.out.println("\nWomen: ");
             womenPreferences.forEach((key, value) -> System.out.println(key + ": " + value));
-        }
+        } 
         scanner.close();
     }
 
 
-         /**
+    /**
      * Started implementing the stable pairing by trying to follow the Gale-Shapley algorithm. 
      * Currently it gives a result, but seems to be incorrect.
      *
+     * @param output Decideds if extra information should be printed out or not in the terminal.
+     * 
      */
     private void algorithmGS(boolean output){
         ArrayList<Integer> p = new ArrayList<>(menPreferences.keySet());
         Map<Integer,Integer> pairs = new HashMap<>();
 
- 
+        
+        //Time compexity n-men in p
         while(!p.isEmpty()){
                     int man = p.remove(0);
                     ArrayList<Integer> womansPrefered = menPreferences.get(man);
-                    
-                    //int firstWoman = womansPrefered.remove(0);
 
+                    //Time compexity n-women in preferences => in total n^2.
                     for(int firstWoman : womansPrefered){
                         if (!pairs.containsKey(firstWoman)){
                             pairs.put(firstWoman, man);
@@ -88,6 +88,8 @@ public class StableMarriage{
                         }
                     }
         }
+
+        //prints out extra information if "output" was sent in the arg when program runned.
         if (output){
             pairs.forEach((key,value) -> {
                 System.out.println("Woman: " + key + ", Man: " + value);
